101c224bfbf51669c33c05272d14d4f130c0615c
govindthakur23 <montygovind9@gmail.com>
2023-07-27 16:36:16 +0530

    project initialize

--- NewsApp/src/App.js
+++ NewsApp/src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 import react, { Component } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   render() {  // scren pe html ko render krna
     return (
       <div>
-      <Navbar />
-       <News />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<News key="general" pageSize={5} country="in" category="general" />} />
+            <Route path="/business"  element={<News key="business" pageSize={5} country="in" category="business" />} />
+            <Route path="/entertainment"  element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />} />
+            <Route path="/general"  element={<News key="general" pageSize={5} country="in" category="general" />} />
+            <Route path="/health"  element={<News key="health" pageSize={5} country="in" category="health" />} />
+            <Route path="/science"  element={<News key="science" pageSize={5} country="in" category="science" />} />
+            <Route path="/sports"  element={<News key="sports" pageSize={5} country="in" category="sports" />} />
+            <Route path="/technology"  element={<News key="technology" pageSize={5} country="in" category="technology" />} />
+          </Routes>
+        </Router>
       </div>
     )
-  }  
+  }
 }
 
 
--- NewsApp/src/components/Newsitem.js
+++ NewsApp/src/components/Newsitem.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
 
   render() {
-    let {title , descriptioin , imageUrl , newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date ,source } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card" >
+          <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '90%' ,zIndex: '1' }}>  {source}  </span>
+          <img src={imageUrl ? imageUrl : "https://c.ndtvimg.com/2023-07/kmgd8hko_nitish-rana-and-rinku-singh-bcci_625x300_06_July_23.jpg?im=FitAndFill,algorithm=dnn,width=1200,height=675"} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{descriptioin}</p>
-            <a href="#" className="btn btn-sm btn-primary">Read More</a>
+            <h5 className="card-title">{title} </h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
